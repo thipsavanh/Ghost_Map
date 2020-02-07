635ed49ac4c55c2cdf56a231a7f30cb1dd688a2a
Michael Connell <mjconl6291@gmail.com>
2020-02-06 19:08:59 -0600

    can't figure out the maps docs

--- javascript.js
+++ javascript.js
@@ -1,2 +1,9 @@
+var apiKey = "AIzaSyDv1x2CyzV8gMN5jvG0An_acinRQ-TF5IA"
+var queryURL = "https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap"
 
-
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
