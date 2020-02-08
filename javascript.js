a24ff53093dc65e3a3c4ef2953753273c4f21512
philipwking <42879808+philipwking@users.noreply.github.com>
2020-02-08 12:42:37 -0600

    Deleted extra comments
    changed ghost icon

--- javascript.js
+++ javascript.js
@@ -13,17 +13,16 @@ console.log(data);
   for(i=0;i<data.length;i++){
       var latLng = new google.maps.LatLng(data[i].latitude,data[i].longitude);
       var icon = {
-        url: "https://image.flaticon.com/icons/svg/477/477155.svg",
+        url: "https://image.flaticon.com/icons/svg/1030/1030345.svg",
         scaledSize: new google.maps.Size(20,20),
         origin: new google.maps.Point(0,0),
         anchor: new google.maps.Point(0,0)
       };
-      var newPin = new google.maps.Marker({
+      new google.maps.Marker({
         position: latLng,
         map: map,
         icon: icon,
       });
-      console.log(newPin)
   }
 });
 
@@ -33,67 +32,9 @@ console.log(data);
 var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 41.8781, lng: -87.6298},
-    zoom: 9
+    center: {lat: 41.875754, lng: -87.623912},
+    zoom: 11
   });
   infoWindow = new google.maps.InfoWindow;
-
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);
-    }, function () {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
-
-
-
-// var latitude = 30
-// var longitude = -90
-// var distance = 500
-// var page = 1
-
-
-// var queryURL =
-//   "https://api.crimeometer.com/v1/incidents/crowdsourced-raw-data?city_key=&lat=" + latitude + "&lon=" + latitude + "&distance=" + distance + "mi&datetime_ini=2010-01-07T00:00:00.000Z&datetime_end=2020-02-06T00:00:00.000Z&page=2"
-// // "https://api.crimeometer.com/v1/incidents/raw-data-coverage"
-
-// console.log(queryURL)
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-//   headers: {
-//     'Content-Type': 'application/json',
-//     "x-api-key": "k3syy8bIYK9vFJwwn43zH3dGhrUPPbLya49c1hi8"
-//   },
-//   crossDomain: false
-// }).then(function (response) {
-//   console.log(response)
-//   for (i = 0; response.incidents.length; i++) {
-//     console.log(i)
-//     console.log(response.incidents[i].incident_offense)
-//   }
-// })
